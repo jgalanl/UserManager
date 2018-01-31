eb86d8c6a75d69875eb404da3adb63be656911df
jgalanllano <35027233+jgalanllano@users.noreply.github.com>
2018-01-31 10:43:01 +0100

    Initial commit

---
+++ app/src/main/java/com/example/jesusgalan/usermanager/PantallaPresentacion.java
@@ -0,0 +1,36 @@
+package com.example.jesusgalan.usermanager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class PantallaPresentacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_presentacion);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Thread reloj=new Thread(){
+          public void run(){
+              try{
+                  sleep(3000);
+              }
+              catch(InterruptedException e){
+                  e.printStackTrace();
+              }
+              finally{
+                  Intent principal = new Intent("com.example.jesusgalan.usermanager.PantallaPrincipal");
+                  startActivity(principal);
+              }
+          }
+        };
+        reloj.start();
+
+    }
+
+
+}
