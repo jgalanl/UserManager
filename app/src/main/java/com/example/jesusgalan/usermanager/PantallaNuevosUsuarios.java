aaad26e88c1a89ee6c2654cd63d7bf63fa581254
jgalanllano <35027233+jgalanllano@users.noreply.github.com>
2018-02-06 17:05:19 +0100

    Conexion con el servidor web

--- app/src/main/java/com/example/jesusgalan/usermanager/PantallaNuevosUsuarios.java
+++ app/src/main/java/com/example/jesusgalan/usermanager/PantallaNuevosUsuarios.java
@@ -2,6 +2,7 @@ package com.example.jesusgalan.usermanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -9,17 +10,20 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.concurrent.ExecutionException;
 
 public class PantallaNuevosUsuarios extends AppCompatActivity {
 
     Button insertar;
-    Spinner nacionalidad;
+    Spinner nat;
     CheckBox male, female;
     EditText num;
     DatePicker date;
+    String cadena_json;
     //https://randomuser.me/api/?inc=nat,registered,gender,picture,location,&nat=gb,&gender=female,&results=50
+
+   // https://randomuser.me/api/?inc=gender,registered&gender=male&registered=2013-08-26%2004:54:46&noinfo
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +35,11 @@ public class PantallaNuevosUsuarios extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //Reocoger datos introducidos por el usuario
-                nacionalidad = findViewById(R.id.spinner_nacionalidad);
-                String nat = nacionalidad.getSelectedItem().toString();
-                String genero;
+                //Nacionalidad
+                nat = findViewById(R.id.spinner_nacionalidad);
+                String nacionalidad = nat.getSelectedItem().toString();
+                //Genero
+                String genero="Por defecto";
                 male = findViewById(R.id.box_male);
                 female = findViewById(R.id.box_female);
                 if (male.isChecked()){
@@ -42,16 +48,30 @@ public class PantallaNuevosUsuarios extends AppCompatActivity {
                 if(female.isChecked()){
                     genero = "female";
                 }
+                //Numero de usuarios a insertar
                 num = findViewById(R.id.editText);
                 String numero = num.getText().toString();
+                Log.d("holi", "numero"+numero);
+                //Fecha
                 date = findViewById(R.id.datePicker);
-                int   day  = date.getDayOfMonth();
-                int   month= date.getMonth();
-                int   year = date.getYear();
+                int day  = date.getDayOfMonth();
+                int month= date.getMonth();
+                int year = date.getYear();
                 Calendar calendar = Calendar.getInstance();
                 calendar.set(year, month, day);
 
-                SimpleDateFormat sdf = new SimpleDateFormat("DD-MM-YYYY");
+                //SimpleDateFormat sdf = new SimpleDateFormat("DD-MM-YYYY");
+
+                //Obtener respuesta del servidor
+                ObtenerJSON hilo = new ObtenerJSON();
+                try {
+                    cadena_json = hilo.execute(nacionalidad, genero, numero).get();
+                } catch (InterruptedException | ExecutionException e) {
+                    e.printStackTrace();
+                }
+                Log.d("holi", "nuevos usuarios"+cadena_json);
+
+                //Insertar respuesta en la bbdd
 
 
             }
