9dcbed502809cff50130f69526251e4521969a02
Calorain <35992877+Calorain@users.noreply.github.com>
2018-04-26 21:25:44 +0200

    KeyStore FIN?

--- app/src/main/java/com/example/jesusgalan/usermanager/KeystoreProvider.java
+++ app/src/main/java/com/example/jesusgalan/usermanager/KeystoreProvider.java
@@ -2,6 +2,7 @@ package com.example.jesusgalan.usermanager;
 
 import android.content.Context;
 import android.security.KeyPairGeneratorSpec;
+import android.util.Base64;
 import android.util.Log;
 
 import java.io.IOException;
@@ -29,8 +30,6 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.security.auth.x500.X500Principal;
 
-import static com.example.jesusgalan.usermanager.Crypto.fromBase64;
-import static com.example.jesusgalan.usermanager.Crypto.toBase64;
 
 
 public class KeystoreProvider {
@@ -126,4 +125,12 @@ public static void loadKeyStore(){
             throw new RuntimeException(e);
         }
     }
+
+    public static byte[] fromBase64(String base64) {
+        return Base64.decode(base64, Base64.NO_WRAP);
+    }
+    public static String toBase64(byte[] bytes) {
+        return Base64.encodeToString(bytes, Base64.NO_WRAP);
+    }
+
 }
